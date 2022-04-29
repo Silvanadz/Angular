735ea39d4a0c861ecb6ee8a78f5a3f08e79d21b7
Silvana De Zan <silvanaadezan@gmail.com>
2022-04-29 11:43:09 -0300

    agregue section

--- mi-nuevo-portfolio-angular/src/app/app.module.ts
+++ mi-nuevo-portfolio-angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HeaderComponent
+    HeaderComponent,
+    SectionComponent
   ],
   imports: [
     BrowserModule,
